2dd42054fec298d434ca562d1924ee07ce8ed4e8
VRavindu <vimukthiravindu01@gmail.com>
2024-10-06 09:59:06 +0530

    Customer Saving - init level

--- src/main/java/lk/ijse/gdse/possystembackendspring/controller/CustomerController.java
+++ src/main/java/lk/ijse/gdse/possystembackendspring/controller/CustomerController.java
@@ -1,4 +1,18 @@
 package lk.ijse.gdse.possystembackendspring.controller;
 
+import lk.ijse.gdse.possystembackendspring.dto.CustomerDTO;
+import lk.ijse.gdse.possystembackendspring.util.AppUtil;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/customer")
 public class CustomerController {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> createCustomer(@RequestBody CustomerDTO customer) {
+        customer.setId(AppUtil.CreateCustomerId());
+        System.out.println(customer);
+        return ResponseEntity.ok("Customer Saved Successfully");
+    }
 }
---
+++ src/main/java/lk/ijse/gdse/possystembackendspring/util/AppUtil.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse.possystembackendspring.util;
+
+import java.util.UUID;
+
+public class AppUtil {
+    public static String CreateCustomerId(){
+        return "CUSTOMER : " + UUID.randomUUID();
+    }
+}
